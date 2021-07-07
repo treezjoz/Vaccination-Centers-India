9f3b858978d243fbc506324a9c5a16c4890fb636
Treesa Jose <treesajisjoy3280@gmail.com>
2021-07-07 09:46:40 +0530

    private route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import Login from "./Login";
-import Otp from "./Otp";
 import Display from "./Display";
 import "./css/styles.css";
 
 export default function App() {
+  const PrivateRoute = ({ component: Component, ...rest }) =>
+    (
+      <Route
+        {...rest}
+        render={props =>
+          sessionStorage.getItem('token') ?
+          (<Component {...props} />) :
+          (<Redirect to={{pathname: "/"}}/>)}/>
+    );
+
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/" exact component={Login} />
-          <Route path="/enter-otp/:txnId"  component={Otp} />
-          <Route path="/display/:token" component={Display} />
+          <PrivateRoute path="/display" component={Display} />
         </Switch>
       </div>
     </Router>
-
   );
 };
