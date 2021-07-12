1d6c2f0939cd79ec62f0c62a7808e38770be063b
Treesa Jose <treesajisjoy3280@gmail.com>
2021-07-12 08:34:46 +0530

    loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './css/loader.css'
+const App = React.lazy(() => import('./App'));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={
+      <div class="around">
+        <div class="spinner"></div>
+      </div>
+      }>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
